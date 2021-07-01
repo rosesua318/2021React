fecf221f058a2981d5356315dccac2abafe8cff8
rosesua318 <rosesua@duksung.ac.kr>
2021-07-01 15:36:21 +0900

    Chapter 8

---
+++ hooks/src/Info.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  /*
+  useEffect(() => {
+    console.log("렌더링이 완료되었습니다!");
+    /*console.log({
+      name,
+      nickname,
+    });
+  }, []);*/
+
+  /*
+  useEffect(() => {
+    console.log(name);
+  }, [name]); // name이 없데이트 될 때만 실행*/
+
+  useEffect(() => {
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  }, [name]);
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
