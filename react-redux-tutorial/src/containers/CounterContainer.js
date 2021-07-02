92e69c51ebd3c3ee20b6c955a8ca6ccb4bfc9f7b
rosesua318 <rosesua@duksung.ac.kr>
2021-07-02 18:32:42 +0900

    Chapter 17

---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,66 @@
+import React, { useCallback } from 'react';
+import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+/*
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};*/
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+export default CounterContainer;
+
+/*
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+});
+const mapDispatchToProps = (dispatch) => ({
+  increase: () => {
+    //console.log('increase');
+    dispatch(increase());
+  },
+  decrease: () => {
+    //console.log('decrease');
+    dispatch(decrease());
+  },
+});*/
+//export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+/*
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  /*
+  (dispatch) => ({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease()),
+  }),*/
+/*
+  (dispatch) =>
+    bindActionCreators(
+      {
+        increase,
+        decrease,
+      },
+      dispatch,
+    ),
+  {
+    increase,
+    decrease,
+  },
+)(CounterContainer);*/
